b02638604b62e0897f2d4fdc61cebca82dc73c9a
Karim Gharbi <Mirak005>
2020-01-16 16:49:54 +0100

    first commit

---
+++ client/src/gg.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class test extends Component {
+    render() {
+        return (
+            <div>
+
+                fdthyjklhgfd
+                fdghjkhgfgh
+                fgh,gfdghhjgfdfhjjhkjggfdsfgh
+                dfyghhjklm
+
+            </div>
+        )
+    }
+}
